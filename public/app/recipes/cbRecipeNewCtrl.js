919c283f9749b7f1ff8769f0cd8324dadb46ebb7
Postcert <postcert@gmail.com>
2015-04-20 17:59:26 -0400

    Recipe details additions

---
+++ public/app/recipes/cbRecipeNewCtrl.js
@@ -0,0 +1,3 @@
+angular.module('app').controller('cbRecipeNewCtrl', function($scope, cbAuth, sbIdentity, cbNotify) {
+    $scope.title =
+})
--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,11 +1,17 @@
 var mongoose = require('mongoose');
 
+var maxHour = [24, 'The value of field `{PATH}` ({VALUE}) exceeds the limit ({MAX}).'];
+var maxMinute = [60, 'The value of field `{PATH}` ({VALUE}) exceeds the limit ({MAX}).'];
+
 var recipeSchema = mongoose.Schema({
     title: {type: String, required:'{PATH} is required'},
     creator: {type: String, required:'{PATH} is required'},
     featured: {type:Boolean, required:'{PATH} is required'},
     published: {type:Date, required:'{PATH} is required'},
     cookbook: {type: String, required:'{PATH} is required'},
+    cooktimeHour: {type: Number, required:'{PATH} is required', max: maxHour},
+    cooktimeMinute: {type: Number, require:'{PATH} is required', max: maxMinute},
+    directions: [String],
     ingredients: [String]
 });
 
@@ -14,8 +20,8 @@ var Recipe = mongoose.model('Recipe', recipeSchema);
 function createDefaultRecipes() {
     Recipe.find({}).exec(function(err, collection) {
         if(collection.length === 0) {
-            Recipe.create({title: 'Chicken', creator: 'Daniel', featured: true, published: new Date('10/5/2013'), cookbook: 'Daniel', ingredients: ['Chicken', 'Fire']});
-            Recipe.create({title: 'Beef', creator: 'Daniel', featured: true, published: new Date('10/5/2013'), cookbook: 'Daniel', ingredients: ['Chicken', 'Fire']});
+            Recipe.create({title: 'Chicken', creator: 'Daniel', featured: true, published: new Date('10/5/2013'), cookbook: 'Daniel', cooktimeHour: 1, cooktimeMinute: 0, directions:['Place Chicken on Flame', 'Eat the Chicken'], ingredients: ['Chicken', 'Fire']});
+            Recipe.create({title: 'Beef', creator: 'Daniel', featured: true, published: new Date('10/5/2013'), cookbook: 'Daniel', cooktimeHour: 1, cooktimeMinute: 0, directions:['Place Chicken on Flame', 'Eat the Chicken'], ingredients: ['Chicken', 'Fire']});
         }
     })
 }
