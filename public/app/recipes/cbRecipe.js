bec93b4d54364e361bdeb77f13d11526dff74098
Postcert <postcert@gmail.com>
2015-04-20 23:07:44 -0400

    Well I really did add a bug

--- public/app/recipes/cbRecipe.js
+++ public/app/recipes/cbRecipe.js
@@ -1,9 +1,9 @@
 angular.module('app').factory('cbRecipe', function($resource) {
     return {
-        cookbooks: $resource('/api/recipes/:_id', {_id: "@id"}, {
+        recipes: $resource('/api/recipes/:_id', {_id: "@id"}, {
             update: {method: 'GET', isArray: true}
         }),
-        cookbooksuser: $resource('/api/recipesuser/:user_id', {user_id: "@user_id"}, {
+        recipesuser: $resource('/api/recipesuser/:user_id', {user_id: "@user_id"}, {
             update: {method: 'GET', isArray: true}
         })
     }
